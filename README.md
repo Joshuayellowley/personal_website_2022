0b6164bfd4c5e2c2e6cf9a5fde35563d67b81b30
Joshua Yellowley <joshuayellowley@Joshuas-MacBook-Pro.local>
2021-12-29 17:20:20 -0800

    Initial Commit of BoilerPlate

---
+++ README.md
@@ -0,0 +1,43 @@
+# 👨‍💻 Personal DevFolio (2.0)
+
+### _Personal Portfolio built using ReactJs and Animation libraries, hope you like it. Tweak it and use it !!_
+
+
+
+### Demo Video (2.0) :link: https://youtu.be/soi-rnv9dDQ
+
+### Link :link: https://developerjunaid.com/
+
+## Images
+
+<img src='./project_images/home.png' />
+<img src='./project_images/navbar.png' />
+<img src='./project_images/services.png' />
+<img src='./project_images/portfolio.png' />
+<img src='./project_images/testimonials.png' />
+<img src='./project_images/contact.png' />
+
+## Mobile Responsive
+
+<img src='./project_images/mobile.png' />
+
+## Run Locally
+
+- Run this command `git clone https://github.com/developer-junaid/DeveloperFolio.git`
+- You are now in the dev environment and you can play around
+
+## Tech Stack
+
+- HTML5
+- CSS3
+- Javascript ES6
+- React js
+
+- Animation Libraries
+- - react-scroll
+- - aos
+- - custom css animation
+- - framer-motion
+
+- Firebase
+- Github Actions
---
+++ src/data/data.js
@@ -0,0 +1,227 @@
+export const services = [
+  {
+    title: "Frontend Development",
+    iconSrc:
+      "https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "Sass",
+      "ReactJs",
+      "GatsbyJs",
+      "NextJs",
+      "Tailwind-css",
+      "Material-UI",
+      "Bootstrap",
+      "Firebase",
+    ],
+  },
+  {
+    title: "MERN Stack Development",
+    iconSrc: "https://cdn.worldvectorlogo.com/logos/mern.svg",
+    tags: [
+      "MongoDB",
+      "ExpressJs",
+      "ReactJs",
+      "NodeJs",
+      "REST",
+      "Typescript",
+      "NextJs",
+      "Stripe",
+      "Firebase",
+    ],
+  },
+  {
+    title: "JAM Stack Development",
+    iconSrc:
+      "https://d33wubrfki0l68.cloudfront.net/1d6248982d56f5b390b078f8ad053c3a6e61bfb2/ef690/img/logo/svg/jamstack_icon_original.svg",
+    tags: [
+      "NextJs",
+      "GatsbyJs",
+      "GraphQl",
+      "FaunaDB",
+      "NextJs",
+      "Contentful",
+      "Strapi",
+      "Typescript",
+      "Serverless Functions",
+      "Firebase",
+    ],
+  },
+  {
+    title: "Excel to Web Conversion",
+    iconSrc:
+      "https://img.icons8.com/external-flatart-icons-flat-flatarticons/2x/external-excel-taxes-flatart-icons-flat-flatarticons.png",
+    tags: ["Excel", "HTML", "CSS", "Javascript", "Typescript", "ReactJs"],
+  },
+];
+
+export const portfolioList = [
+  {
+    id: "featured",
+    title: "Featured",
+  },
+  {
+    id: "webApp",
+    title: "Web App",
+  },
+  {
+    id: "website",
+    title: "Website",
+  },
+];
+
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Diaries Manager",
+    img: "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs2/202080094/original/5f338db02a4293531252ebee42b0410895c80dfa/develop-react-js-redux-scss-firebase-web-app-or-website.png",
+    repositoryUrl: "https://github.com/developer-junaid/Diaries-Manager-App",
+    liveUrl: "https://diaries-manager.web.app/",
+    tagline: "Productivity / Management",
+  },
+  {
+    id: 2,
+    title: "TechBerg Blog",
+    img: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+    repositoryUrl: "https://github.com/developer-junaid/Blog-Site/",
+    liveUrl: "https://techberg-blog.netlify.app/",
+    tagline: "Blog / Marketing",
+  },
+  {
+    id: 3,
+    title: "Ecommerce Store",
+    img: "https://images.unsplash.com/photo-1582004531564-50f300aae039?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZWNvbW1lcmNlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    repositoryUrl: "https://github.com/developer-junaid/Buy-It",
+    liveUrl: "https://buyit-shoes.netlify.app/",
+    tagline: "Ecommerce / Store",
+  },
+  {
+    id: 4,
+    title: "Memories Social Media App",
+    img: "https://images.unsplash.com/photo-1534131707746-25d604851a1f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+    repositoryUrl: "https://github.com/developer-junaid/MERN-Memories-App",
+    liveUrl: "https://memories-manager.netlify.app/",
+    tagline: "Social Media / Management",
+  },
+];
+
+export const webAppPortfolio = [
+  {
+    id: 1,
+    title: "Diaries Manager",
+    img: "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs2/202080094/original/5f338db02a4293531252ebee42b0410895c80dfa/develop-react-js-redux-scss-firebase-web-app-or-website.png",
+    repositoryUrl: "https://github.com/developer-junaid/Diaries-Manager-App",
+    liveUrl: "https://diaries-manager.web.app/",
+    tagline: "Productivity / Management",
+  },
+  {
+    id: 2,
+    title: "Memories Social Media App",
+    img: "https://images.unsplash.com/photo-1534131707746-25d604851a1f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80",
+    repositoryUrl: "https://github.com/developer-junaid/MERN-Memories-App",
+    liveUrl: "https://memories-manager.netlify.app/",
+    tagline: "Social Media / Management",
+  },
+];
+
+export const websitePortfolio = [
+  {
+    id: 1,
+    title: "TechBerg Blog",
+    img: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80",
+    repositoryUrl: "https://github.com/developer-junaid/Blog-Site/",
+    liveUrl: "https://techberg-blog.netlify.app/",
+    tagline: "Blog / Marketing",
+  },
+  {
+    id: 2,
+    title: "Shoe Store",
+    img: "https://images.unsplash.com/photo-1518002171953-a080ee817e1f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHNob2VzJTIwc2hvcHxlbnwwfDB8MHx8&auto=format&fit=crop&w=500&q=60",
+    repositoryUrl: "https://github.com/developer-junaid/Nike-Shoe-Store",
+    liveUrl: "https://nike-shoe-store.netlify.app/",
+    tagline: "Ecommerce / Store",
+  },
+  {
+    id: 3,
+    title: "Ecommerce Store",
+    img: "https://images.unsplash.com/photo-1582004531564-50f300aae039?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8ZWNvbW1lcmNlfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+    repositoryUrl: "https://github.com/developer-junaid/Buy-It",
+    liveUrl: "https://buyit-shoes.netlify.app/",
+    tagline: "Ecommerce / Store",
+  },
+];
+
+// Testimonials
+export const fiverrTestimonials = [
+  // {
+  //   id: 1,
+  //   text: "text",
+  //   country: "name",
+  //   name: "name",
+  //   url: "https://www.fiverr.com/developerjunaid/",
+  // },
+  {
+    id: 1,
+    text: "Great communication skills, very good english and easy to work with! thanks for the excellent work and I hope we will work together in the future again!",
+    country: "Italy",
+    name: "Leo Matter",
+    url: "https://www.fiverr.com/developerjunaid/",
+  },
+  {
+    id: 2,
+    text: "Junaid represents among the highest levels of customer service I have experienced. Information was accurate, responses to queries were turned around very fast. Answers were clear and where necessary detailed enough for us to make informed decisions quickly. Thanks so much we will be working with you in future.",
+    country: "Pakistan",
+    name: "Arslan",
+    url: "https://www.fiverr.com/developerjunaid/",
+  },
+  {
+    id: 3,
+    text: "Very professional and know what he is doing, will be working with you again in the future!",
+    country: "Belgium",
+    name: "Walid",
+    url: "https://www.fiverr.com/developerjunaid/",
+  },
+  {
+    id: 4,
+    text: "this developer is the one of a kind with great communication skills!",
+    country: "Denmark",
+    name: "dgaa1991",
+    url: "https://www.fiverr.com/developerjunaid/",
+  },
+  {
+    id: 5,
+    text: "Perfect man for your job",
+    country: "Cyprus",
+    name: "worldwidefame",
+    url: "https://www.fiverr.com/developerjunaid/",
+  },
+  {
+    id: 6,
+    text: "From my very first message to last, the seller communicated everything clearly and asked all the right questions unlike others asking for budget. The code is very clean (easy to maintain and good commenting). Very quick delivery, ill be ordering again soon. Thanks again!!",
+    country: "UK",
+    name: "Ahmed",
+    url: "https://www.fiverr.com/developerjunaid/",
+  },
+  {
+    id: 7,
+    text: "2nd order done already, one of the quickest delivery on Fiverr. Again, task was done alot better than expected, thank you!",
+    country: "UK",
+    name: "Ahmed",
+    url: "https://www.fiverr.com/developerjunaid/",
+  },
+  {
+    id: 8,
+    text: "Great service, fast and responsive!",
+    country: "Belgium",
+    name: "Walid",
+    url: "https://www.fiverr.com/developerjunaid/",
+  },
+  {
+    id: 9,
+    text: "good work, thank you",
+    country: "Belgium",
+    name: "Walid",
+    url: "https://www.fiverr.com/developerjunaid/",
+  },
+];
